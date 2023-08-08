a695d6dddf38a96d271fdd39354529946f901d1f
Ervan Ade Wijaya <ervanade26@gmail.com>
2023-08-08 13:26:53 +0700

    fix

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -5,28 +5,32 @@ import { BsCart } from "react-icons/bs";
 const Hero = () => {
   return (
     <div className="hero relative w-screen h-screen overflow-hidden">
-      <div className="overlay w-full h-full absolute top-0 left-0 bg-black bg-opacity-20 z-1"></div>
-      <div className="text absolute top-36 sm:left-16 left-10  lg:left-40 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg flex flex-col space-y-10">
-        <div className="text flex flex-col space-y-8">
-          <h1 className="text-4xl tracking-[4px] leading-[40px] lg:text-5xl font-bold lg:tracking-[12px] lg:leading-[60px]">
-            SKINARO TEA BRIGHTENING CLEANSER
-          </h1>
-          <p className="text-lg tracking-[2px] font-normal">
-            Facial cleansing soap containing Tea Extract & Glycolic Acid helps
-            brighten and cleanse dead skin while calming the skin.
-          </p>
-        </div>
-        <div className="cta">
-          <button className="items-center justify-center py-5 px-8 bg-[#6B826F] rounded-full flex flex-row space-x-2 hover:bg-white hover:text-[#6B826F]">
-            <BsCart className="text-xl" /> <span>Shop Now</span>
-          </button>
+      <div className="overlay w-full h-full absolute top-0 left-0 bg-black bg-opacity-20 z-[2]"></div>
+      <div className="container mx-auto max-w-7xl px-6 md:px-10 lg:px-24 py-4 w-full h-full relative bg-transparent z-10" >
+        <div className="text absolute top-36 sm:left-16 left-10 lg:left-40  left-50 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg flex flex-col space-y-10">
+          <div className="text flex flex-col space-y-8">
+            <h1 className="text-4xl tracking-[4px] leading-[40px] lg:text-5xl font-bold lg:tracking-[12px] lg:leading-[60px]">
+              SKINARO TEA BRIGHTENING CLEANSER
+            </h1>
+            <p className="text-lg tracking-[2px] font-normal">
+              Facial cleansing soap containing Tea Extract & Glycolic Acid helps
+              brighten and cleanse dead skin while calming the skin.
+            </p>
+          </div>
+          <div className="cta">
+            <button className="items-center justify-center py-5 px-8 bg-[#6B826F] rounded-full flex flex-row space-x-2 hover:bg-white hover:text-[#6B826F]">
+              <BsCart className="text-xl" /> <span>Shop Now</span>
+            </button>
+          </div>
         </div>
       </div>
+
       <img
         src="/hero-skinaro.png"
-        className="h-full lg:w-full object-cover"
+        className="h-full lg:w-full object-cover absolute top-0 left-0"
         alt="Hero Banner"
       />
+
     </div>
   );
 };
--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -3,15 +3,15 @@ import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 const Navbar = () => {
-    const [color, setColor] = useState(false);
-    const [menuButton, setMenuButton] = useState(false)
+  const [color, setColor] = useState(false);
+  const [menuButton, setMenuButton] = useState(false)
 
-    const buttonOnChangeBars = () => {
-        setMenuButton(prev => !prev)
-      }
-      const buttonOnChangeClose = () => {
-        setMenuButton(false)
-      }
+  const buttonOnChangeBars = () => {
+    setMenuButton(prev => !prev)
+  }
+  const buttonOnChangeClose = () => {
+    setMenuButton(false)
+  }
   const changeColor = () => {
     if (typeof window !== "undefined") {
       // Client-side-only code
@@ -26,50 +26,49 @@ const Navbar = () => {
     window.addEventListener("scroll", changeColor);
   }
   const navbarMenu = [
-    {name: "Home", link: "/"},
-    {name: "Why Aro ?", link: "/"},
-    {name: "Our Product ?", link: "/"},
-    {name: "Talk With Us", link: "/"},
+    { id: 1, name: "Home", link: "/" },
+    { id: 2, name: "Why Aro ?", link: "/" },
+    { id: 3, name: "Our Product ?", link: "/" },
+    { id: 4, name: "Talk With Us", link: "/" },
   ]
   return (
     <div
-    className={`${
-      color ? "bg-white text-[#6B826F]" : ""
-    } navbar-fixed fixed top-0 left-0 w-full z-50`}
-  >
-    <div className="container mx-auto max-w-7xl px-6 md:px-10 lg:px-24 py-4 flex justify-between items-center">
-    <div className="left-navbar">
-      {color ? (
-        <img src="/skinaro-logo-hitam.png" alt="" className="w-28 h-14" />
-      ) : (
-        <img src="/skinaro_white_green.png" alt="" className="w-28 h-14" />
-      )}
-    </div>
-    <button className="lg:hidden" onClick={buttonOnChangeBars}>
-      <GiHamburgerMenu className={`text-2xl ${color ? 'text-[#6B826F] hover:text-[#5e8a65]' : 'text-gray-200 hover:text-white'} `} />
-    </button>
-    <div className="right-navbar space-x-6 hidden lg:block">
-        {navbarMenu.map(item => {
-            return(
-            <a href={item.link} className={`${color ? 'text-[#6B826F] hover:text-[#5e8a65]' : 'text-gray-200 hover:text-white '} text-xl ${item.name === 'Home' ? 'font-bold' : ''} ${item.name === 'Home' && !color ? '!text-white' : ''}`}>
-            {item.name}
-          </a>
+      className={`${color ? "bg-white text-[#6B826F]" : ""
+        } navbar-fixed fixed top-0 left-0 w-full z-50`}
+    >
+      <div className="container mx-auto max-w-7xl px-6 md:px-10 lg:px-24 py-4 flex justify-between items-center">
+        <div className="left-navbar">
+          {color ? (
+            <img src="/skinaro-logo-hitam.png" alt="" className="w-28 h-14" />
+          ) : (
+            <img src="/skinaro_white_green.png" alt="" className="w-28 h-14" />
+          )}
+        </div>
+        <button className="lg:hidden" onClick={buttonOnChangeBars}>
+          <GiHamburgerMenu className={`text-2xl ${color ? 'text-[#6B826F] hover:text-[#5e8a65]' : 'text-gray-200 hover:text-white'} `} />
+        </button>
+        <div className="right-navbar space-x-6 hidden lg:block">
+          {navbarMenu.map(item => {
+            return (
+              <a key={item.id} href={item.link} className={`${color ? 'text-[#6B826F] hover:text-[#5e8a65]' : 'text-gray-200 hover:text-white '} text-xl ${item.name === 'Home' ? 'font-bold' : ''} ${item.name === 'Home' && !color ? '!text-white' : ''}`}>
+                {item.name}
+              </a>
             )
-        })}
-    
-    </div>
-    <div className={`right-navbar space-y-4 flex flex-col fixed top-0 ${menuButton ? 'right-0' : '-right-[350px]'} right-0 lg:hidden w-[350px] py-16 px-14 bg-[#9faca0] text-white h-full duration-200 transition-all ease-in`}>
-        <button className='absolute top-5 right-6' onClick={buttonOnChangeClose}><GiHamburgerMenu className='text-3xl text-gray-200 hover:text-white ' /></button>
-        {navbarMenu.map(item => {
-            return(
-            <a href={item.link} className={`text-xl text-gray-200 hover:text-white font-medium `}>
-            {item.name}
-          </a>
+          })}
+
+        </div>
+        <div className={`right-navbar space-y-4 flex flex-col fixed top-0 ${menuButton ? 'right-0' : '-right-[350px]'} lg:hidden w-[350px] py-16 px-14 bg-[#9faca0] text-white h-full duration-200 transition-all ease-in`}>
+          <button className='absolute top-5 right-6' onClick={buttonOnChangeClose}><GiHamburgerMenu className='text-3xl text-gray-200 hover:text-white ' /></button>
+          {navbarMenu.map(item => {
+            return (
+              <a key={item.id} href={item.link} className={`text-xl text-gray-200 hover:text-white font-medium `}>
+                {item.name}
+              </a>
             )
-        })}
+          })}
+        </div>
+      </div>
     </div>
-    </div>
-  </div>
   )
 }
 
