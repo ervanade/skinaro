6e24065dc617f0f1666e6695de7d64287c77a2b1
Ervan Ade Wijaya <ervanade26@gmail.com>
2023-08-08 22:36:14 +0700

    add arrow slider

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ export default function Home() {
     <>
       <Head>
         <link rel="icon" href="/favicon-skinaro.png" />
+        <title>Skinaro - Beauty Aro Tea Skin Care</title>
       </Head>
       <Navbar />
 
