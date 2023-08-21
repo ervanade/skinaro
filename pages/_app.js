2dea6db149ac0730228b81a318a4226421d4b530
Ervan Ade Wijaya <eawijaya@cpxi-asia.com>
2023-08-21 12:30:41 +0700

    fix script

--- pages/_app.js
+++ pages/_app.js
@@ -38,19 +38,6 @@ function MyApp({ Component, pageProps }) {
           `;
   // const [apiScript, setApiScript] = useState('');
 
-  // Ambil data script dari API (contoh: hardcoded untuk demonstrasi)
-  // const apiScriptData = `<script type="text/javascript" async="" src="https://www.googletagmanager.com/gtag/js?id=DC-10827733&amp;l=dataLayer&amp;cx=c"></script>`;
-  // const apiScriptData = `!function(f,b,e,v,n,t,s)
-  // {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  // n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  // if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  // n.queue=[];t=b.createElement(e);t.async=!0;
-  // t.src=v;s=b.getElementsByTagName(e)[0];
-  // s.parentNode.insertBefore(t,s)}(window, document,'script',
-  // 'https://connect.facebook.net/en_US/fbevents.js');
-  // fbq('init', '${"1486056371919786"}');
-  // fbq('track', 'PageView');`;
-
   // useEffect(() => {
   //   setApiScript(scriptString);
   // }, []);
