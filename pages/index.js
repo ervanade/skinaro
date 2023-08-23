d0deb9d736c8698e75637335740d108daa7166cd
Ervan Ade Wijaya <eawijaya@cpxi-asia.com>
2023-08-23 15:43:32 +0700

    fix gap

--- pages/index.js
+++ pages/index.js
@@ -38,10 +38,10 @@ export default function Home() {
       {/* Section Benefit */}
       <div className="points-skinaro w-full flex flex-wrap justify-center items-center py-8 lg:my-16 container mx-auto max-w-7xl gap-12 bg-cover bg-no-repeat">
 
-        <img src="assets/Icon/Icon-1.png" alt="" className="w-2/6 sm:w-auto sm:max-w-[150px] " />
-        <img src="assets/Icon/Icon-2.png" alt="" className="w-2/6 sm:w-auto sm:max-w-[150px]" />
-        <img src="assets/Icon/Icon-4.png" alt="" className="w-2/6 sm:w-auto sm:max-w-[150px]" />
-        <img src="assets/Icon/Icon-5.png" alt="" className="w-2/6 sm:w-auto sm:max-w-[150px]" />
+        <img src="assets/Icon/Icon-1.png" alt="" className="w-2/6 sm:w-auto sm:max-w-[150px] px-2 sm:px-0 " />
+        <img src="assets/Icon/Icon-2.png" alt="" className="w-2/6 sm:w-auto sm:max-w-[150px] px-2 sm:px-0" />
+        <img src="assets/Icon/Icon-4.png" alt="" className="w-2/6 sm:w-auto sm:max-w-[150px] px-2 sm:px-0" />
+        <img src="assets/Icon/Icon-5.png" alt="" className="w-2/6 sm:w-auto sm:max-w-[150px] px-2 sm:px-0" />
       </div>
 
       <Product />
